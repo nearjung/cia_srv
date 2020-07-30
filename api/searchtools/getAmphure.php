ba56f91b0e2380e1fa3124ab506d476af0c97f62
Marvis <oens_4@hotmail.com>
2020-07-31 01:18:09 +0700

    Update

--- api/searchtools/getAmphure.php
+++ api/searchtools/getAmphure.php
@@ -4,12 +4,15 @@ header("Access-Control-Allow-Origin: *");
 include("../../configuration/config.php");
 
 
+$province = $_GET['province'];
+
 // Response
 $return['id'] = '';
 $return['code'] = 200;
 $return['status'] = "Success";
 $return['text'] = "Load Success.";
-$back_sql = $sql->prepare("SELECT ProvinceThai FROM trthailandinformation GROUP BY ProvinceThai ORDER BY ProvinceThai");
+$back_sql = $sql->prepare("SELECT DistrictThaiShort FROM trthailandinformation WHERE ProvinceThai = :province GROUP BY DistrictThaiShort ORDER BY DistrictThaiShort");
+$back_sql->BindParam(":province", $province);
 $back_sql->execute();
 while($back = $back_sql->fetch(PDO::FETCH_ASSOC)) {
     $return['value'][] = $back;
--- api/searchtools/getTambon.php
+++ api/searchtools/getTambon.php
@@ -4,12 +4,15 @@ header("Access-Control-Allow-Origin: *");
 include("../../configuration/config.php");
 
 
+$amphure = $_GET['amphure'];
+
 // Response
 $return['id'] = '';
 $return['code'] = 200;
 $return['status'] = "Success";
 $return['text'] = "Load Success.";
-$back_sql = $sql->prepare("SELECT ProvinceThai FROM trthailandinformation GROUP BY ProvinceThai ORDER BY ProvinceThai");
+$back_sql = $sql->prepare("SELECT TambonThaiShort FROM trthailandinformation WHERE DistrictThaiShort = :amphure GROUP BY TambonThaiShort ORDER BY TambonThaiShort");
+$back_sql->BindParam(":amphure", $amphure);
 $back_sql->execute();
 while($back = $back_sql->fetch(PDO::FETCH_ASSOC)) {
     $return['value'][] = $back;
